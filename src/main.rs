3dd18960628b513ed2cf40e3b28d1778eeb5649c
Ethan Water <watermonic@gmail.com>
2025-03-10 00:04:27 -0400

    +experimental:verbose_async (much faster, but ugly display)

--- src/main.rs
+++ src/main.rs
@@ -364,6 +364,178 @@ fn linecount_verbose(
     Ok((total_lines, total_bytes))
 }
 
+//EXPERIMENTAL: runs linecount_verbose via parrellization. has significant increase in speed.
+//   -BUGS: since the function operates in parrell, printing the treemap is unreliable since order is not guaranteed. 
+//          because of this the output looks scattered and disorganized.
+//
+//use std::thread;
+//use std::sync::{Arc, Mutex};
+//
+//fn linecount_verbose_async(
+//    dir: Option<PathBuf>,
+//    byte_toggle: bool,
+//    ignore_toggle: bool,
+//    mut indent_amount: Option<usize>,
+//) -> Result<(u128, u128)> {
+//    let total_lines = Arc::new(Mutex::new(0));
+//    let total_bytes = Arc::new(Mutex::new(0));
+//    let dir_path_binding = dir.unwrap_or(env::current_dir()?);
+//    let dir_path = dir_path_binding.as_path();
+//    let mut file_indent_from_zero_size = indent_amount.unwrap_or_default();
+//    let ignore_vec = fetch_gitignore(&dir_path)?;
+//    let mut handles = Vec::new();
+//
+//    if indent_amount.is_none() {
+//        indent_amount = Some(0);
+//    } else if indent_amount.unwrap() > 0 {
+//        file_indent_from_zero_size += 1;
+//    }
+//
+//    let (dir_indent, file_indent_from_dir, file_ident_from_zero) = (
+//        "─".repeat(indent_amount.unwrap_or_default()),
+//        "─".repeat(2),
+//        " ".repeat(file_indent_from_zero_size),
+//    );
+//    let dir_path_str = dir_path
+//        .file_name()
+//        .unwrap()
+//        .to_str()
+//        .unwrap_or_default()
+//        .blue()
+//        .bold();
+//
+//    match indent_amount {
+//        Some(0) => println!("{dir_indent}{dir_path_str}/"),
+//        _ => println!("├{dir_indent}{dir_path_str}/"),
+//    }
+//
+//    let entries = fs::read_dir(dir_path)
+//        .expect("Failed to read directory")
+//        .map(|entry| entry.unwrap().path())
+//        .collect::<Vec<_>>();
+//    let (mut files, mut dirs) = (Vec::new(), Vec::new());
+//
+//    for entry in entries {
+//        //if ignore_toggle {
+//        //    if ignore_vec.contains(&entry.file_name().unwrap().to_string_lossy().to_string()) {
+//        //        continue;
+//        //    }
+//        //}
+//
+//        if entry.is_file() {
+//            files.push(entry);
+//        } else {
+//            dirs.push(entry);
+//        }
+//    }
+//    files.sort();
+//    dirs.sort();
+//    let sorted_entries = files.iter().chain(dirs.iter());
+//
+//    for (idx, entry) in sorted_entries.enumerate() {
+//        let mut connector = "├";
+//        let path = entry.as_path();
+//        let filetype = fs::metadata(path)?.file_type();
+//
+//        if filetype.is_file() {
+//            let content = String::from_utf8_lossy(&fs::read(&path)?).into_owned();
+//            let file_linecount = content.lines().count() as u128;
+//            
+//            let mut file_bytes: u128 = 0;
+//
+//            *total_lines.lock().unwrap() += file_linecount;
+//            if byte_toggle {
+//                file_bytes = content.as_bytes().len() as u128;
+//                *total_bytes.lock().unwrap() += file_bytes;
+//            }
+//
+//            if entry.is_visible() && !ignore_vec.contains(&entry.file_name().unwrap().to_string_lossy().to_string()) {
+//                let filename = entry
+//                .file_name()
+//                .unwrap()
+//                .to_str()
+//                .unwrap_or("?")
+//                .to_string();
+//
+//                let filename = if filename.len() > FILENAME_RENDER_LIMIT {
+//                    format!("{}...", &filename[..FILENAME_RENDER_LIMIT])
+//                } else {
+//                    filename
+//                };
+//                if idx == files.len() - 1 {
+//                    connector = "└";
+//                }
+//
+//                let formatted_indent = match indent_amount {
+//                    Some(0) => format!("{file_ident_from_zero}{connector}{file_indent_from_dir}"),
+//                    _ => format!("|{file_ident_from_zero}{connector}{file_indent_from_dir}"),
+//                };
+//
+//                let formatted_output = match byte_toggle {
+//                    true => format!(
+//                        "{:width$} ({}L, {}B)",
+//                        {
+//                            match path.content_type() {
+//                                ContentType::MEDIA => filename.bright_magenta().to_string(),
+//                                ContentType::CODE => filename.cyan().to_string(),
+//                                ContentType::EXECUTABLE => filename.green().to_string(),
+//                                ContentType::TEXT => filename.truecolor(217, 50, 122).to_string(),
+//                                ContentType::LICENSE => filename.truecolor(0,0,255).to_string(),
+//                                ContentType::MAKEFILE => filename.red().to_string(),
+//                                _ => filename.to_string(),
+//                            }
+//                        },
+//                        file_linecount,
+//                        file_bytes,
+//                        width = WIDTH
+//                    ),
+//                    false => format!(
+//                        "{:width$} (lines: {})",
+//                        filename,
+//                        file_linecount,
+//                        width = WIDTH
+//                    ),
+//                };
+//                println!("{formatted_indent}{formatted_output}");
+//            }
+//        } else if filetype.is_dir() {
+//            let handle = {
+//                let total_lines = Arc::clone(&total_lines);
+//                let total_bytes = Arc::clone(&total_bytes);
+//                let path = PathBuf::from(path);
+//        
+//                thread::spawn(move || {
+//                    let recursive_lc = linecount_verbose(
+//                        Some(path),
+//                        byte_toggle,
+//                        ignore_toggle,
+//                        Some(indent_amount.unwrap() + 2),
+//                    );
+//        
+//                    if let Ok((lines, bytes)) = recursive_lc {
+//                        *total_lines.lock().unwrap() += lines;
+//                        if byte_toggle {
+//                            *total_bytes.lock().unwrap() += bytes;
+//                        }
+//                    }
+//                })
+//            };
+//            handles.push(handle);
+//        }
+//    }
+//    for handle in handles {
+//        handle.join().unwrap();
+//    }
+//
+//    Ok(get_totals(total_lines, total_bytes))
+//}
+//
+//fn get_totals(total_lines: Arc<Mutex<u128>>, total_bytes: Arc<Mutex<u128>>) -> (u128, u128) {
+//    let lines = *total_lines.lock().unwrap();  // Extract the value
+//    let bytes = *total_bytes.lock().unwrap();  // Extract the value
+//    (lines, bytes) // Return as a tuple of u128, not Arc<Mutex<u128>>
+//}
+
 fn format_byte_count(byte_count: u128) -> String {
     if byte_count / 1_000_000_000 > 1 {
         return format!("{} GB", byte_count as f64 / 1_000_000_000.);
